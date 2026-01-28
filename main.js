951451e1acbaccac6316014897d495dc2edfe3af
Daniel <dijalmaduartefleitas0@gmail.com>
2026-01-27 21:44:35 -0400

    Corrigindo transição

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ let btnEnviar = document.querySelector(".enviar");
 let corpoLista = document.querySelector(".lista");
 let cardsCriados = localStorage.getItem("cards") ? JSON.parse(localStorage.getItem("cards")) : [];
 let btnLimpar = document.querySelector(".limpar");
+let navegacao = document.querySelectorAll(".botao-nav");
+let containerCalculadora = document.querySelector(".calculadora");
+let formularioToList = document.querySelector(".formulario");
+let cards = document.querySelector(".cards");
 
-console.log(cardsCriados);
+
 
 
 btnLimpar.addEventListener("click", function(){
@@ -26,7 +30,7 @@ function criarCards(dadosCard){
                                     <div class="card-body">
                                         <p class="card-text">
                                         <p class="alert alert-info">Adicionado no horario</p>
-                                            ${dadosCard.hora}
+                                            ${dadosCard.hora} 
                                         </p>
                                             <button class="btn btn-danger w-100 excluir-card">Excluir</button>
                                     </div>
@@ -39,8 +43,14 @@ function criarCards(dadosCard){
 btnEnviar.addEventListener("click", function(event){
     event.preventDefault();
 
+
     let valorInput = document.querySelector("#textouser").value;
     
+    if(valorInput === ""){
+        alert("Preencha o campo")
+        return;
+    }
+
     let dataAtual = new Date();
     let horaCriado = String(dataAtual.getHours()).padStart(2, '0') + ":" + String(dataAtual.getMinutes()).padStart(2, '0');
 
@@ -72,3 +82,35 @@ corpoLista.addEventListener("click", function(event){
         cardClicado.parentElement.remove();
     }
 })
+navegacao.forEach(button => {
+    button.addEventListener("click",function(event){
+        let secaoClicado = event.target.dataset.secao;
+        
+        if(secaoClicado === "home"){
+            containerCalculadora.classList.add("desativo");
+            setTimeout(function(){
+                containerCalculadora.style.display = "none";
+            },800)
+            formularioToList.style.display = ""; // Remove o display: none anterior
+            formularioToList.classList.remove("desativo");
+            formularioToList.classList.add("ativo");
+            
+            cards.style.display = "";
+            cards.classList.remove("desativo");
+
+        }
+        else if(secaoClicado === "calculadora"){
+            formularioToList.classList.add("desativo");
+            cards.classList.add("desativo");
+
+            setTimeout(function(){
+                formularioToList.style.display = "none";
+                cards.style.display = "none";
+                },800)
+            containerCalculadora.style.display = "";
+            containerCalculadora.classList.add("ativo");
+        }
+        
+        
+    })
+});
